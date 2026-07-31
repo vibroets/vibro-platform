cb71e9c1ffaa84b11b3bf2c7dc210a23b5d5d45b
Vibro Admin <vibro.chennai@gmail.com>
2026-07-31 22:20:57 +0530

    Add whitenoise for serving static files in production

--- vibro-backend-main/vibro/settings.py
+++ vibro-backend-main/vibro/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -261,6 +262,15 @@ EMAIL_HOST_USER = 'vibro.chennai@gmail.com'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
