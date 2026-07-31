fee3c78d0cdc949aa28649b0ae40526e2498f9c0
Vibro Admin <vibro.chennai@gmail.com>
2026-07-31 21:24:43 +0530

    Add STATIC_ROOT for production collectstatic

--- vibro-backend-main/vibro/settings.py
+++ vibro-backend-main/vibro/settings.py
@@ -260,6 +260,7 @@ EMAIL_HOST_USER = 'vibro.chennai@gmail.com'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
