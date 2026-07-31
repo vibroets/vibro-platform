5eedafc73d1a4dd7b62b54516245d694875a76f9
Vibro Admin <vibro.chennai@gmail.com>
2026-07-31 22:24:18 +0530

    Fix: use CompressedStaticFilesStorage to avoid manifest errors

--- vibro-backend-main/vibro/settings.py
+++ vibro-backend-main/vibro/settings.py
@@ -268,7 +268,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
